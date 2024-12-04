ae66c93e1126f8e562996238f505c2a3c48cc25f
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2024-12-05 00:04:37 +0700

    diep

--- frontend/src/page/FilmDetail.jsx
+++ frontend/src/page/FilmDetail.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../component/Header/Header'
 import Footer from '../component/Footer/Footer'
+import FilmInfo from '../Film/FilmInfo'
 export default function FilmDetail() {
   return (
     <>
     <Header></Header>
-
-    <Footer></Footer>
+    <FilmInfo/>
+    {/* <Footer></Footer> */}
     </>
   )
 }
